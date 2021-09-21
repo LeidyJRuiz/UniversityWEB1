b936126d88caca84ebeec66fe76d165e37187731
LeidyJRuiz <leidy.ruiz.0ds@utap.edu.co>
2021-09-20 21:34:33 -0500

    CLASE 20/09/2021

--- University.API/Global.asax.cs
+++ University.API/Global.asax.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,14 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using University.BL.DTOs;
 
 namespace University.API
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+
+        internal static MapperConfiguration MapperConfiguration { get; set; }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -18,6 +22,8 @@ namespace University.API
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            MapperConfiguration = MapperConfig.MapperConfiguration();
         }
     }
 }
---
+++ University.BL/DTOs/MapperConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using University.BL.Models;
+
+namespace University.BL.DTOs
+{
+    public class MapperConfig
+    {
+        public static MapperConfiguration MapperConfiguration()
+        {
+
+            //MODEL (DB)-> DTO
+            //DTO -> MODEL(DB)
+
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Student, StudentDTO>();
+                cfg.CreateMap<StudentDTO, Student>();
+
+                cfg.CreateMap<Course, CourseDTO>();
+                cfg.CreateMap<CourseDTO, Course>();
+            });
+
+        }
+
+    }
+}
